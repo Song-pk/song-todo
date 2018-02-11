e63b55453699bafdb1d8264dca771abe182a72ed
Song Myoung Su <audtn0508@naver.com>
2018-02-11 23:01:48 +0900

    Coding the UI part five - To Do Component

--- ToDo.js
+++ ToDo.js
@@ -1,15 +1,25 @@
 import React, { Component } from "react";
-import { View, Text, TouchableOpacity, StyleSheet, Dimensions } from "react-native";
+import { View, Text, TouchableOpacity, StyleSheet, Dimensions, TextInput } from "react-native";
+import PropTypes from "prop-types";
 
 const { width, height } = Dimensions.get("window");
 
+
 export default class ToDo extends Component{
-    state = {
-        isEditing: false,
-        isCompleted: false
+    constructor(props) {
+        super(props);
+        this.state = { isEditing: false, toDoValue: props.text}
+    }
+    static PropTypes = {
+        text: PropTypes.string.isRequired, 
+        isCompleted: PropTypes.bool.isRequired,
+        deleteToDo: PropTypes.func.isRequired,
+        id: PropTypes.string.isRequired
     };
+   
     render() {
-        const { isCompleted, isEditing } = this.state;
+        const { isCompleted, isEditing, toDoValue } = this.state;
+        const { text, id, deleteToDo } = this.props;
         return(
         <View style={styles.container}>
             <View style={styles.column}>
@@ -19,9 +29,19 @@ export default class ToDo extends Component{
                 isCompleted ? styles.completedCircle : styles.uncompletedCircle
             ]} />
         </TouchableOpacity>
-            <Text style={[styles.text,
-                 isCompleted ? styles.completedText: styles.uncompletedText]}>
-            Hello I'm a To Do</Text>
+            {isEditing ? (
+            <TextInput 
+            style={[styles.text, styles.input]} 
+            value={toDoValue} 
+            multiline={true}
+            onChangeText={this._controllInput}
+            returnKeyType={"done"}
+            onBlur={this._finishEditing} />
+            ) : (
+                <Text style={[styles.text,
+                    isCompleted ? styles.completedText: styles.uncompletedText]}>
+               {text}</Text>
+            )}
             </View>
 
                 {isEditing ? (
@@ -39,7 +59,7 @@ export default class ToDo extends Component{
                             <Text style={styles.actionText}>✏️</Text>
                         </View>
                     </TouchableOpacity>
-                    <TouchableOpacity>
+                    <TouchableOpacity onPressOut={() => deleteToDo(id) }>
                         <View style={styles.actionContainer}>
                             <Text style={styles.actionText}>❌</Text>
                         </View>
@@ -67,6 +87,9 @@ export default class ToDo extends Component{
             isEditing: false
         });
     };
+    _controllInput = (text) => {
+        this.setState({ toDoValue: text})
+    }
 }
 
 
@@ -107,8 +130,7 @@ const styles = StyleSheet.create({
     column: {
         flexDirection: "row",
         alignItems: "center",
-        width: width /2,
-        justifyContent: "space-between"
+        width: width /2
     },
     actions: {
         flexDirection: "row"
@@ -116,5 +138,9 @@ const styles = StyleSheet.create({
     actionContainer: {
         marginVertical: 10,
         marginHorizontal: 10
+    },
+    input: {
+        marginVertical: 15,
+        width: width / 2
     }
 });
